2e2f3f8af372bd4158e52bb542407b72e1389507
tahabayi <tahabayi@gmail.com>
2017-03-25 04:35:43 +0300

    Card class generated

---
+++ app/src/main/java/com/hackathon/getir/bigezsek/Card.java
@@ -0,0 +1,88 @@
+package com.hackathon.getir.bigezsek;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.mindorks.placeholderview.SwipePlaceHolderView;
+import com.mindorks.placeholderview.annotations.Click;
+import com.mindorks.placeholderview.annotations.Layout;
+import com.mindorks.placeholderview.annotations.Resolve;
+import com.mindorks.placeholderview.annotations.View;
+import com.mindorks.placeholderview.annotations.swipe.SwipeCancelState;
+import com.mindorks.placeholderview.annotations.swipe.SwipeIn;
+import com.mindorks.placeholderview.annotations.swipe.SwipeInState;
+import com.mindorks.placeholderview.annotations.swipe.SwipeOut;
+import com.mindorks.placeholderview.annotations.swipe.SwipeOutState;
+
+/**
+ * Created by taha on 25/03/17.
+ */
+
+@Layout(R.layout.card_view)
+public class Card {
+
+    @View(R.id.profileImageView)
+    private ImageView profileImageView;
+
+    @View(R.id.nameAgeTxt)
+    private TextView nameAgeTxt;
+
+    @View(R.id.locationNameTxt)
+    private TextView locationNameTxt;
+
+    private Profile mProfile;
+    private Context mContext;
+    private SwipePlaceHolderView mSwipeView;
+
+    public Card(Context context, Profile profile, SwipePlaceHolderView swipeView) {
+        mContext = context;
+        mProfile = profile;
+        mSwipeView = swipeView;
+    }
+
+    @Click(R.id.profileImageView)
+    private void click(){
+        CharSequence text = "PP clicked!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(mContext, text, duration);
+        toast.show();
+    }
+
+    @Resolve
+    private void onResolved(){
+        Glide.with(mContext).load(mProfile.getImageUrl()).into(profileImageView);
+        nameAgeTxt.setText(mProfile.getName() + ", " + mProfile.getAge());
+        locationNameTxt.setText(mProfile.getLocation());
+    }
+
+    @SwipeOut
+    private void onSwipedOut(){
+        Log.d("EVENT", "onSwipedOut");
+        mSwipeView.addView(this);
+    }
+
+    @SwipeCancelState
+    private void onSwipeCancelState(){
+        Log.d("EVENT", "onSwipeCancelState");
+    }
+
+    @SwipeIn
+    private void onSwipeIn(){
+        Log.d("EVENT", "onSwipedIn");
+    }
+
+    @SwipeInState
+    private void onSwipeInState(){
+        Log.d("EVENT", "onSwipeInState");
+    }
+
+    @SwipeOutState
+    private void onSwipeOutState(){
+        Log.d("EVENT", "onSwipeOutState");
+    }
+}
