1df83953d00fcef59bf438850b85642780465893
Melih <m.melihmutlu@gmail.com>
2017-03-25 04:31:21 +0300

    action search bar

--- app/src/main/java/com/hackathon/getir/bigezsek/MainActivity.java
+++ app/src/main/java/com/hackathon/getir/bigezsek/MainActivity.java
@@ -1,8 +1,12 @@
 package com.hackathon.getir.bigezsek;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -12,9 +16,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
 public class MainActivity extends AppCompatActivity {
 
     Button searchButton;
+    Toolbar toolbar;
+    MaterialSearchView searchView;
     Context context;
 
     @Override
@@ -22,6 +30,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Search");
+        toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"));
+
+        searchView = (MaterialSearchView) findViewById(R.id.search_view);
+
+        // TODO Change this to fragment
+/*
         searchButton = (Button) findViewById(R.id.search_button);
         context = this;
 
@@ -32,7 +49,15 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
-
+*/
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        searchView.setMenuItem(item);
+        return true;
+    }
 }
