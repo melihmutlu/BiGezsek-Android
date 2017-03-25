d00554a458473c1ae8689b304a00ff96db0fc016
Melih <m.melihmutlu@gmail.com>
2017-03-25 06:22:45 +0300

    place search on toolbar

--- app/src/main/java/com/hackathon/getir/bigezsek/MainActivity.java
+++ app/src/main/java/com/hackathon/getir/bigezsek/MainActivity.java
@@ -1,6 +1,5 @@
 package com.hackathon.getir.bigezsek;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
@@ -14,17 +13,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.Button;
 
 import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button searchButton;
     Toolbar toolbar;
-    MaterialSearchView searchView;
     Context context;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,32 +31,32 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Search");
-        toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"));
 
-        searchView = (MaterialSearchView) findViewById(R.id.search_view);
 
-        // TODO Change this to fragment
-/*
-        searchButton = (Button) findViewById(R.id.search_button);
-        context = this;
-
-        searchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(context, GooglePlacesActivity.class);
-                startActivity(i);
-            }
-        });
-*/
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        MenuItem item = menu.findItem(R.id.action_search);
-        searchView.setMenuItem(item);
         return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_search) {
+            Intent i = new Intent(this, GooglePlacesActivity.class);
+            startActivity(i);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
